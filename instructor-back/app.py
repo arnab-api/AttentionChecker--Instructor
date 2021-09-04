0df4332c30181e8d0b8a84d6e82791abe10a56c4
arnab-api <arnab.api@gmail.com>
2021-09-04 13:52:34 +0600

    backend port change

--- instructor-back/app.py
+++ instructor-back/app.py
@@ -103,6 +103,6 @@ def loadHeatMapFromFullSession():
 if __name__ == "__main__":
     app.run(
         host=os.getenv('IP', '0.0.0.0'), 
-        port=int(os.getenv('PORT', 5555)), 
+        port=int(os.getenv('PORT', 3005)), 
         debug=True
     )
